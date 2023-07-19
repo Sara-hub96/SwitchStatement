c582244daa14d4257e7dbde4f82eb284439bfe40
Sara <saracoppola996@gmail.com>
2023-07-20 00:09:23 +0200

    while loop

--- src/Test.java
+++ src/Test.java
@@ -5,21 +5,48 @@ public class Test {
 
         Scanner consoleInput = new Scanner(System.in);
 
-        //System.out.println(numero);
-
-        while(true){
+        boolean run = true;
+        while(run){
             int numero = consoleInput.nextInt();
+
             switch (numero){
                 case 1 :
                     System.out.println("one");
+                    break;
                 case 2 :
                     System.out.println("two");
+                    break;
                 case 3:
                     System.out.println("three");
+                    break;
+                case 4:
+                    System.out.println("four");
+                    break;
+                case 5:
+                    System.out.println("five");
+                    break;
+                case 6:
+                    System.out.println("six");
+                    break;
+                case 7:
+                    System.out.println("seven");
+                    break;
+                case 8:
+                    System.out.println("eight");
+                    break;
+                case 9:
+                    System.out.println("nine");
+                    break;
+                case 10:
+                    System.out.println("ten");
+                    break;
+                default:
+                    run = false;
+                    System.out.println("Cannot give you the name");
+
             }
 
         }
-
 
     }
 }
